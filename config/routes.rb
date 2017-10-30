b99448aa51a4586d177faa4c8652cf1595d28fee
malinek <malin.ekelund@gmail.com>
2017-10-30 14:17:07 +0100

    4.3 create orders controller & config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
