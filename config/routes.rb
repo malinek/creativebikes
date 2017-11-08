5e3d43b1dca770841adeb36988d96e3f4f15ae3b
malinek <malin.ekelund@gmail.com>
2017-11-08 15:24:11 +0100

    5.1 reroute root page & create dynamic links

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+
+  root 'static_pages#index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  
-  root 'static_pages#landing_page'
 
+  get 'static_pages/landing_page'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
