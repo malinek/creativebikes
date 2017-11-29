e55cdc84d6ce07531d400a91bb52ff22335b7c3d
malinek <malin.ekelund@gmail.com>
2017-11-29 16:28:45 +0100

    5.7 create Comment model, set up scopes and custom methods, style show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,19 @@
 class Product < ActiveRecord::Base
   has_many :orders
+  has_many :comments
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
 end
 
 def Product.search(search_term)
   like_string = Rails.env.production? ? "ilike" : "LIKE"
   Product.where("name #{like_string} ?", "%#{search_term}%")
 end
+
