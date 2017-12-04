096d0e4c2a8085228656ff944af48e95f56eefb3
malinek <malin.ekelund@gmail.com>
2017-12-04 16:00:56 +0100

    5.9 add validations to comment model, implement will_paginate gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
 
+  validates :name, presence: true
+
   def highest_rating_comment
     comments.rating_desc.first
   end
