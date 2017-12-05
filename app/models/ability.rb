44ec698911466ebf77e57f52b61a9170dbbc11f2
malinek <malin.ekelund@gmail.com>
2017-12-05 10:29:29 +0100

    5.10 Add admin role, restrict access to admins for delete function

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin? #additional permissions for administrators
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+    end
   end
 
 end
-
-
-
-
 
     # Define abilities for the passed in user here. For example:
     #
