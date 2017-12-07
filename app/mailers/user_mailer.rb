d0d455c3e6cf21947ea72ad1f76204f7369c821f
malinek <malin.ekelund@gmail.com>
2017-12-07 13:13:13 +0100

    5.11 add automated email on user sign up & some styling user pages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
         to: "malin.ekelund@gmail.com",
         subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  	@appname = "Creative bikes"
+  	mail(to: user.email,
+  		subject: "Welcome to #{@appname}!")
+  end
+
 end
