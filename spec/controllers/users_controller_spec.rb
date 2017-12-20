f722c64e685533fc72dad4199a3a4aa881443084
malinek <malin.ekelund@gmail.com>
2017-12-20 10:54:06 +0100

    6.3 Functional tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   let (:user) { User.create!(email: "nina@hotmail.com", password: "secure") }
+  let (:user2) { User.create!(email: "eva@gmail.com", password: "test23") }
 
   describe 'GET #show' do
     context "When a user is logged in" do
@@ -11,7 +12,7 @@ describe UsersController, type: :controller do
       end
 
       it "loads correct user details" do
-        get :show, params: { id: user.id }
+        get :show, id: user.id
         expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq user
       end
@@ -20,15 +21,26 @@ describe UsersController, type: :controller do
 
     context "When a user is not logged in" do
       it "redirects to login" do
-        get :show, params: { id: user.id }
+        get :show, id: user.id
         expect(response).to redirect_to(new_user_session_path)
       end
 
     end
 
+    context "User cannot see other user show page" do
+      
+      before do
+        sign_in user2
+      end
+
+      it "redirect to root path" do
+        get :show, id: user.id
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
   end
 
 end
-
-
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,5 +59,4 @@ RSpec.configure do |config|
 
   # For Devise >= 4.1.0
   config.include Devise::Test::ControllerHelpers, :type => :controller
-
 end
