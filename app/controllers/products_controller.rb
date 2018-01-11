85a70fe64c0c68ac32cf85b71013f4dfad40e974
malinek <malin.ekelund@gmail.com>
2018-01-11 11:19:21 +0100

    remove byebug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC").paginate(page: params[:page], per_page: 5)
-    byebug
+    #byebug
   end
 
   # GET /products/new
