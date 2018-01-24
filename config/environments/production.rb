d019d60f60c07675fe03b080accabf5a84be1087
malinek <malin.ekelund@gmail.com>
2018-01-24 10:33:13 +0100

    6.11 edit Dalli set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
 
   config.cache_store = :dalli_store,
                       (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                      {:username => ENV["40B97F"],
-                      :password => ENV["29406ECE30091A268F0F6953EDB217E2"],
+                      {:username => ENV["MEMCACHIER_USERNAME"],
+                      :password => ENV["MEMCACHIER_PASSWORD"],
                       :failover => true,
                       :socket_timeout => 1.5,
                       :socket_failure_delay => 0.2,
